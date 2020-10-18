e6750b9ebbc3fabf8cc4ced68c38b3c7b1f9218d
RamyaBhat23 <ramya.shi.1111@gmail.com>
2020-10-18 23:04:48 +0530

    image editor project

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
-const app = express();
-app.use(express.static('./dist/ImageEditor'));
+// var express = require('express');
+// const app = express();
+// app.use(express.static('./dist/ImageEditor'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: './dist/ImageEditor' });
-});
-app.listen(process.env.PORT || 8080, () => {
-    console.log("http://localhost:8080");
-});
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', { root: './dist/ImageEditor' });
+// });
+// app.listen(process.env.PORT || 8080, () => {
+//     console.log("http://localhost:8080");
+// });
